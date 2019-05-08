7a6a99dfc7d88fedad524a2ce1622e3c0cbafec0
Jonathan Schatz <jonathan.schatz@bc.libraries.coop>
2019-05-08 13:08:07 -0700

    Move save unmatched steps to save_unmatched_records() function, add type hints where needed.
    Signed-off-by: Jonathan Schatz <jonathan.schatz@bc.libraries.coop>

--- match_filter.py
+++ match_filter.py
@@ -1,67 +1,40 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
-from typing import List
+from typing import List, Dict, Union
 import click
 import glob
 import re
 import os
 import time
 
+
 @click.command()
 @click.argument('path', type=click.Path(exists=True, file_okay=False))
 @click.argument('marcxml', type=click.File(mode='r+'))
 def process(path, marcxml):
-	process_start = time.time()
-	index = filenames_to_index(path)
+	# process_start = time.time()
 
-	# Dict with length, collection, soup keys
-	parsed = parse_marcxml(marcxml)
+	# Build a filename index from first argument
+	index = filenames_to_index(path)  # type: List[str]
 
-	unmatched_record_ids = diff_index_records(index, parsed['collection'])
+	# Build parsed dict from second argument
+	parsed = parse_marcxml(marcxml)  # type: Dict[str, Union[int, List[str], BeautifulSoup]]
+
+	unmatched_record_ids = diff_index_records(index, parsed['collection'])  # type: List[str]
 	if len(unmatched_record_ids) is not 0:
 		click.echo("Found {} unmatched items to remove".format(len(unmatched_record_ids)))
 	else:
 		click.echo("No unmatched items found. Exiting.")
 		exit(0)
 
-	# @todo move to own function
-	# snip ----
+	save_unmatched_records(unmatched_record_ids, parsed['soup'], marcxml)
 
-	unmatched_records = []
-	with click.progressbar(unmatched_record_ids, label="Collecting unmatchable records from set...") as bar:
-		for u in bar:
-			try:
-				unmatched_records.append(parsed['soup'].find('marc:controlfield', string=u).parent)
-			except AttributeError:
-				click.echo("A parent marc:record element was not found for 001: {}. Skipping...".format(u))
-
-	writeable_unmatched_records = list(map(lambda r: str(r), unmatched_records))
-	unmatched_records_file = os.path.dirname(marcxml.name) + "/" + "{}_unmatchable_{}".format(len(unmatched_record_ids),
-							os.path.split(marcxml.name)[1] if os.path.dirname(marcxml.name) is not '' else marcxml.name)
-
-	with open(unmatched_records_file, 'w') as urf:
-		urf.write(
-			'<?xml version="1.0" encoding="UTF-8" ?><marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
-		with click.progressbar(writeable_unmatched_records, label="Exporting unmatchable records...") as bar:
-			for item in bar:
-				urf.write("{}".format(item))
-		urf.write("</marc:collection>")
-
-	if os.path.exists(unmatched_records_file):
-		click.echo("Created {} in current directory".format(unmatched_records_file))
-		# print('{0:0.1f} second execution'.format(time.time() - process_start))
-
-	# ----snip
-
-	# click.echo("Ready to filter file at: {}".format(os.path.realpath(marcxml.name)))
-
-	# if click.confirm('Do you want to continue?', abort=True):
-	filter_start = time.time()
+	# filter_start = time.time()
 	if create_backup(parsed['len'], marcxml):
 		if remove_unmatched(unmatched_record_ids, parsed['soup'], marcxml):
 			click.echo("Filtered out {} unmatched records from {}".format(len(unmatched_record_ids), marcxml.name))
-			# print('{0:0.1f} second execution'.format(time.time() - filter_start))
+		# print('{0:0.1f} second execution'.format(time.time() - filter_start))
 
 
 def filenames_to_index(directory):
@@ -80,7 +53,7 @@ def parse_marcxml(marcxml_file):
 	if len(check_extra) > 1:
 		check_extra[1].extract()
 
-	scn_collection = []  # type: List[str]
+	scn_collection = []
 	tag_collection = marcxml_soup.find_all('marc:datafield')
 	record_length = len(marcxml_soup.find_all('marc:record'))
 
@@ -106,6 +79,37 @@ def diff_index_records(index, scn_collection):
 	matching_whitelist_uniq = list(dict.fromkeys(matching_record_ids))
 	# Subtract the whitelisted matching records from the record ids in collection, left with unmatched record ids
 	return list(set([str(*d.keys()).strip() for d in scn_collection]) - set(matching_whitelist_uniq))
+
+
+def save_unmatched_records(unmatched_record_ids, soup, marcxml):
+	unmatched_records = []
+	# Locate unmatched records
+	with click.progressbar(unmatched_record_ids, label="Collecting unmatchable records from set...") as bar:
+		for u in bar:
+			try:
+				unmatched_records.append(soup.find('marc:controlfield', string=u).parent)
+			except AttributeError:
+				click.echo("A parent marc:record element was not found for 001: {}. Skipping...".format(u))
+
+	writeable_unmatched_records = list(map(lambda r: str(r), unmatched_records))
+	unmatched_records_file = os.path.dirname(marcxml.name) + "/" + "{}_unmatchable_{}".format(len(unmatched_record_ids),
+																							  os.path.split(
+																								  marcxml.name)[
+																								  1] if os.path.dirname(
+																								  marcxml.name) is not '' else marcxml.name)
+
+	# Write to unmatchable file
+	with open(unmatched_records_file, 'w') as urf:
+		urf.write(
+			'<?xml version="1.0" encoding="UTF-8" ?><marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
+		with click.progressbar(writeable_unmatched_records, label="Exporting unmatchable records...") as bar:
+			for item in bar:
+				urf.write("{}".format(item))
+		urf.write("</marc:collection>")
+
+	if os.path.exists(unmatched_records_file):
+		click.echo("Created {} in current directory".format(unmatched_records_file))
+	# print('{0:0.1f} second execution'.format(time.time() - process_start))
 
 
 def create_backup(length, marcxml):
